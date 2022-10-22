ed4b5d21fc9df8124ec5f1e13732a7954a4c0b0f
Sahil1gupta <70632686+Sahil1gupta@users.noreply.github.com>
2022-10-22 13:55:18 +0530

    TEXTANALYZER FILES ADDED

---
+++ ModuleJs1.mjs
@@ -0,0 +1,9 @@
+import UI,{a,b,d} from './ModuleJs2.mjs'
+console.log(UI);
+console.log(a);
+console.log(b);
+console.log(d);
+
+
+
+//to run this javascript file write in terminal node ModuleJs1.mjs
---
+++ ModuleJs2.mjs
@@ -0,0 +1,9 @@
+const a= "sahil";
+const b= "kunal";
+const c= "rohit";
+const d= "dipanshu";
+
+export default c;  //deafault export 
+export {a};       //named export
+export {b};       //named export
+export {d};      //named export
